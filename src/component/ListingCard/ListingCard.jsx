95bfacfc4c3b105d929c1c60e0c6372c456117e7
Omimi-123 <stephenoretan@gmail.com>
2024-10-05 08:59:31 +0100

    v3

--- src/component/ListingCard/ListingCard.jsx
+++ src/component/ListingCard/ListingCard.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import style from './ListingCard.module.css'
+import { Link } from 'react-router-dom'
 
 
 function ListingCard({ img, title, tagOne, tagTwo, tagThree, tagsize }) {
     return (
         <div className={style.ListingCard}>
-            <a href="/Product_detail"> <img src={img} alt={title} /></a>
-            <a href="/Product_detail"><h3>{title}</h3></a>
+            <Link to="/Product_detail"> <img src={img} alt={title} /></Link>
+            <Link to="/Product_detail"><h3>{title}</h3></Link>
             <div className={style.tags}>
                 <p>{tagOne}</p>
                 <p>{tagTwo}</p>
